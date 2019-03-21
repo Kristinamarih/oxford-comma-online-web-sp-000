4c630436352ca4249fc945afc654867e920fb4f9
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-03-21 00:56:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+# "kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos"
 
+def oxford_comma(array)
+  while array.length == 1 
+  puts array.join
+  while array.length == 2 
+  puts array.join(" and ")
+  while array.length >= 3
+    puts array.join(", " + ", and ")
+  end
+end
+end
 end
