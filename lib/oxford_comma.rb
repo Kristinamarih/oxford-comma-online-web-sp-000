28cc6eab23d64d0cf669330d9cbbec50d0e1984e
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-03-21 01:03:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,12 +2,10 @@
 
 def oxford_comma(array)
   if array.length == 1 
-  puts array.join
+  return array.join
   elsif array.length == 2 
-  puts array.join(" and ")
+  return array.join(" and ")
   elsif array.length >= 3
-    puts array.join(", " + ", and ")
-  else
-    nil
+    return array.join(", " + ", and ")
   end
 end
