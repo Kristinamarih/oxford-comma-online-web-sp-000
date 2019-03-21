e94b7bea825417d30cf9f7780cf6b02522080be8
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-03-21 01:20:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,7 @@ def oxford_comma(array)
   elsif array.length == 2 
   return array.join(" and ")
   elsif array.length == 3
-    return array.join(", ") 
+    puts string = array.join(", ") 
+    string.
   end
 end
